8cb86be9fff64569e04e3ac78f3b24cf8b551ccf
VIVEK Aggarwal <vivek@VIVEKs-MacBook-Air.local>
2024-12-09 00:00:14 +0530

    testing

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,6 @@ import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import AWS from 'aws-sdk';
 dotenv.config();
 
 // Initialize app
